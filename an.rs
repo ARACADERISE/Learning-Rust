8814de2cb07ba78da19e9d6fefe9a0384d5cc354
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2021-05-15 13:51:57 -0500

    Update an.rs

--- an.rs
+++ an.rs
@@ -1,14 +1,37 @@
 #![allow(unused_imports)]
 use std::path::PathBuf;
 use std::fs::File;
+use std::io;
 use std::io::{BufReader, BufRead};
+
+#[derive(Debug)]
+enum IoErrors {
+    IOError(io::Error),
+    EmptyFile,
+    InvalidChar
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
 struct Info {
     path: PathBuf,
     valid_file: Result<PathBuf, String>,
-    file_info: Option<&'static str>
+    file_info: Option<Vec<String>>
+}
+
+impl From<io::Error> for IoErrors {
+    fn from(err: io::Error) -> IoErrors {
+        return IoErrors::IOError(err);
+    }
+}
+
+impl IoErrors {
+    fn empty_file() -> IoErrors {
+        return IoErrors::EmptyFile;
+    }
+    fn invalid_char() -> IoErrors {
+        return IoErrors::InvalidChar;
+    }
 }
 
 trait Functions {
@@ -35,9 +58,79 @@ impl Functions for Info {
     }
 }
 
+#[allow(dead_code)]
+impl Info {
+    fn read_file(&mut self) -> Result<&Self, IoErrors> {
+        match self.valid_file {
+            Ok(ref t) => {
+                let content = BufReader::new(File::open(t)?);
+                
+                let mut file_info: Vec<String> = Vec::new();
+                for line in content.lines() {
+                    file_info.push(line?);
+                }
+                if file_info.len() == 0 {
+                    return Err(IoErrors::empty_file())
+                }
+
+                self.file_info = Some(file_info);
+            },
+            _ => {}
+        }
+
+        return Ok(self);
+    }
+
+    fn print_file_info(&mut self) {
+        match self.read_file() {
+            Ok(ref t) => {
+                match &t.file_info {
+                    Some(f) => {
+                        for i in 0..f.len() {
+                            println!("{}", f[i]);
+                        }
+                    },
+                    _ => {}
+                }
+            },
+            Err(t) => panic!("{:?}", t)
+        }
+    }
+
+    fn check_char(_char: u8) -> Result<char, IoErrors> {
+        match _char {
+            _ if _char < 0x41 || _char > 0x7a => return Err(IoErrors::invalid_char()),
+            _ => {}
+        }
+        return Ok(_char as char);
+    }
+
+    fn gather_all_invalid(&mut self) -> Vec<char> {
+        let mut ar: Vec<char> = Vec::new();
+        match self.read_file() {
+            Ok(ref t) => {
+                match &t.file_info {
+                    Some(f) => {
+                        for i in 0..f.len() {
+                            for x in f[i].chars() {
+                                match Info::check_char(x as u8) {
+                                    Ok(_) => {},
+                                    Err(_) => ar.push(x)
+                                }
+                            }
+                        }
+                    },
+                    _ => {}
+                }
+            
+            },
+            Err(t) => panic!("{:?}", t)
+        }
+        return ar;
+    }
+}
+
 fn main() {
     let mut info = Info::new();
-    info.assign(PathBuf::from("."), "main.rss".to_string());
-    
-    println!("{:?}", info);
+    info.assign(PathBuf::from("."), "main.rs".to_string());
 }
